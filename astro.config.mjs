dc19016f1e6e26f95a41ad8b202be6c494696a1e
Matt Pahuta <mhpahuta@gmail.com>
2026-08-31 16:53:02 -0700

    Add fonts and tailwind, remove legacy components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  fonts: [{
+    provider: fontProviders.fontsource(),
+    name: "Outfit",
+    cssVariable: "--font-outfit",
+  }, {
+    provider: fontProviders.fontsource(),
+    name: "Playfair Display",
+    cssVariable: "--font-playfair",
+  }]
+});
